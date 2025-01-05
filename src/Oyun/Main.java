3a535025724cb2b3592be4a00f6877a32a9aa8d5
furkan-uzmez <furkan.n41@hotmail.com>
2025-01-05 18:58:47 +0300

    Delete src directory

--- src/Oyun/Main.java
+++
@@ -1,8 +0,0 @@
-package Oyun;
-import sample.Arayuz;
-import java.io.*;
-public class Main{
-    public static void main (String[] args) throws IOException {
-        Arayuz.launch(Arayuz.class,args);
-    }
-}
